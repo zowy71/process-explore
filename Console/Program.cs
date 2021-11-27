c590a8f8e638dc15f3bfe513dc8b5caaa3a3824d
Clément PERROT <clement.perrot@etudiant.univ-reims.fr>
2021-11-27 11:37:42 +0100

    Commit

--- Console/Program.cs
+++ Console/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
 
 namespace Console
 {
@@ -6,11 +11,38 @@ namespace Console
     {
         static void Main(string[] args)
         {
-            
+            Process notepad = new Process();
+            notepad.StartInfo.FileName = "notepad.exe";
+            notepad.StartInfo.Arguments = "test.txt";
+            notepad.Start();
+
+            do {
+
+                notepad.Refresh();
+                Thread.Sleep(5000);
+                System.Console.WriteLine($"Mémoire consommée : {notepad.WorkingSet64}");
+
+            } while (!notepad.HasExited);
+
+            Process[] All = Process.GetProcesses();
+            foreach (Process proc in All)
+            {
+                System.Console.WriteLine(proc);
+            }
+
+            System.Console.WriteLine($"Pic maximum : {notepad.PeakWorkingSet64}");
+            System.Console.WriteLine("closed");
+
+            /*
+            Process nav = new Process ();
+            nav.StartInfo.FileName = "Chrome.exe";
+            nav.StartInfo.Arguments = "www.google.fr";
+            nav.StartInfo.UseShellExecute = true;
+            nav.StartInfo.WindowStyle = ProcessWindowStyle.Maximized;
+            nav.Start();
+            */
+
         }
-
-
-
 
     }
 }
